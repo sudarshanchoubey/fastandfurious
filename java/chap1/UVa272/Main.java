ad40fcee818518ce19905d56924f10e9b2be31ec
sudarshanchoubey <sudarshan.choubey@gmail.com>
2017-04-16 20:19:00 +0530

    Solved UVa272

--- java/chap1/UVa272/Main.java
+++ java/chap1/UVa272/Main.java
@@ -1,13 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Console;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in).useDelimiter(;
-        while (sc.hasNext()) {
-            char[] token = sc.next("[^\"]*\"[^\"]*\"").toCharArray();
-            int first = 0;
-            for (int i = 0; i < token.length; i++) {
-                if (token[i] == '"') {
-                    if(first == 0) {
-            
-
+    public static void main(String[] args) throws IOException {
+        
+        int c;
+        while((c = System.in.read()) != -1) {
+            if (c == '"') {
+                System.out.format("``");
+                boolean found = true;
+                while(found && (c = System.in.read()) != -1) {
+                    if (c == '"') {
+                        System.out.format("''");
+                        found = false;
+                    } else {
+                        System.out.write(c);
+                    }
+                }
+            } else {
+                System.out.write(c);
+            }
+        }
+    }
+}
